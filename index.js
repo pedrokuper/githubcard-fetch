2963e8687d6b5a5c158ef47cd334f33dbbe4ca3a
Pedro <pkupersmit@gmail.com>
2020-06-09 19:38:26 -0300

    avatar styled.

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 //DOM REFERENCES
 
 let avatarElement = document.querySelector(".js-avatar");
+let avatarLinkElement = document.getElementById("img-link");
 let usernameElement = document.querySelector(".js-username");
-let avatarLink = document.getElementById("img-link");
+let usernameLinkElement = document.getElementById("js-user-link");
 let nameElement = document.querySelector(".js-name");
 let locationElement = document.querySelector(".js-location");
 let repoElement = document.querySelector(".js-repos");
 let followersElement = document.querySelector(".js-followers");
 let inputElement = document.querySelector(".js-input");
 let btnElement = document.querySelector(".js-btn-search");
+
+let cardElement = document.querySelector(".js-card-container")
 //FUNCTIONS
 
-let githubUser = inputElement.value;
+
+
 
 async function getData() {
-    const response = await fetch(`https://api.github.com/users/${githubUser}`);
+
+    let githubUser = inputElement.value;
+    let response = await fetch(`https://api.github.com/users/${githubUser}`);
     const githubData = await response.json();
     console.log(githubData)
 
     cardData();
+    showcard();
 
-    async function cardData() {
+
+    function cardData() {
+
 
         usernameElement.innerHTML = githubData.login;
         nameElement.innerHTML = githubData.name;
         avatarElement.src = githubData.avatar_url;
-        avatarLink.href = `https://github.com/${githubUser}`
-        avatarLink.target = "_blank"
+        avatarLinkElement.href = `https://github.com/${githubUser}`
+        avatarLinkElement.target = "_blank"
+        usernameLinkElement.href = `https://github.com/${githubUser}`
+        usernameLinkElement.target = "_blank"
+        repoElement.innerHTML = githubData.public_repos;
         followersElement.innerHTML = githubData.followers;
+        locationElement.innerHTML = githubData.location;
     }
+
+    function showcard() {
+        inputElement.value = "";
+        cardElement.style.display = "flex";
+
+    }
+
+
 }
 
 
